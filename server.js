ea376a3e7d5eb94f5d12c9c19fab5e8d1fc40a56
DariusRain <dariusrain.student@careerdevs.com>
2020-06-23 09:19:34 -0400

    Created a readline program that will run a particular server

--- server.js
+++ server.js
@@ -1,4 +1,18 @@
 // Server.js
-const server = require('./practice/6-22-bh-b-e-setup/server');
+let server;
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+rl.question("Run practice/", (directory) => {
+  switch (directory) {
+    case "6-22-bh-b-e-setup":
+      server = require("./practice/6-22-bh-b-e-setup/server");
+      return server();
 
-server();
+    default:
+      console.log(`No server found in practice/${directory}`);
+      return false;
+  }
+});
