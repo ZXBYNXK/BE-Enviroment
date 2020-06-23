97441378b388cab2eb655ffe628bc0d42c2fd293
DariusRain <dariusrain.student@careerdevs.com>
2020-06-23 08:51:06 -0400

    Attempting to fix package.json & server.js file to be flexible with imported code from o/ projects

--- practice/6-22-bh-b-e-setup/server.js
+++ practice/6-22-bh-b-e-setup/server.js
@@ -1,20 +1,24 @@
 // Server.js
-const express = require("express");
-const app = express();
+module.exports = () => {
+  const express = require("express");
+  const app = express();
 
-const helmet = require("helmet");
-app.use(helmet());
+  const helmet = require("helmet");
+  app.use(helmet());
 
-const cors = require("cors");
-app.use(cors());
+  const cors = require("cors");
+  app.use(cors());
 
-app.use(express.json());
+  app.use(express.json());
 
-const userRouter = require("./routes/api/users");
-app.use("/api/users", userRouter);
+  const userRouter = require("./routes/api/users");
+  app.use("/api/users", userRouter);
 
-const connectDatabase = require("./config/connectDatabase");
-connectDatabase();
+  const connectDatabase = require("./config/connectDatabase");
+  connectDatabase();
 
-const { port } = require("./config");
-app.listen(port || process.env.PORT, () => console.log("Server Listening..."));
+  const { port } = require("./config");
+  app.listen(port || process.env.PORT, () =>
+    console.log("Server Listening...")
+  );
+};
---
+++ server.js
@@ -0,0 +1,4 @@
+// Server.js
+const server = require('./practice/6-22-bh-b-e-setup/server');
+
+server();
